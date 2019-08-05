272bfa7016c8a31b224395e0749764307dbe3b2c
Grazorus <53581281+Grazorus@users.noreply.github.com>
2019-08-05 09:01:09 +0300

    Add files via upload

--- EquationHomeworkVB.java
+++ EquationHomeworkVB.java
@@ -1,18 +1,25 @@
 package Homework.Lesson_1;
 
 public class EquationHomeworkVB {
+    public static void main(String[] args) {
+        Examples_1();
+    }
 
-    public static void Example_1(int a, int b, int c) {
+    private static void Examples_1() {
+        int a = 3;
+        int b = -14;
+        int c = -5;
         double D = Math.pow(b, 2) - 4 * a * c;
+        System.out.println("Дискриминант =" + D);
         if (D == 0) {
             double x = -b / (2 * a);
             System.out.println(x);
         }
         if (D > 0) {
             double x1 = (-b + Math.sqrt(D)) / (2 * a);
-            System.out.println(x1);
+            System.out.println("x1= " + x1);
             double x2 = (-b - Math.sqrt(D)) / (2 * a);
-            System.out.println(x2);
+            System.out.println("x2= " + x2);
         } else {
             System.out.println("No Solution");
         }
